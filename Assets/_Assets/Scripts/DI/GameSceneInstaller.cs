f72d8e61155fb54cf9baab926edd98e816091b67
Bodix <bodix321@gmail.com>
2025-08-09 08:55:30 +0300

    Setup environment

---
+++ Assets/_Assets/Scripts/DI/GameSceneInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Game.DI
+{
+    public class GameSceneInstaller : MonoInstaller
+    {
+        [SerializeField]
+        private Player _player;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(_player);
+        }
+    }
+}
